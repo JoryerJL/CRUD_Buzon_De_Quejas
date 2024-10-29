6f2705f62d9271e17ffe5ce81e333543fd09a251
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-10-28 20:42:01 -0600

    start mensajes app

--- buzonDeQuejas/settings.py
+++ buzonDeQuejas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p65xm0mzh&pjiv+bk2mhg1wqlz-&!m^1+p$xq55c_ro27v!088
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mensajes.apps.MensajesConfig'
 ]
 
 MIDDLEWARE = [
